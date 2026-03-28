6f645fb5f3d11dbe97a1677e8564c3d537a700a2
LeboKR <leborachomanyane@gmail.com>
2026-03-28 23:39:18 +0200

    Implement contact form with validation and styling updates

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
 document.getElementById("copyrightDate").textContent = new Date().getFullYear();
+
+document.getElementById("contactForm").addEventListener("submit", function(event) {
+    event.preventDefault();
+    /*TODO: Code to store user credentials*/
+    }
